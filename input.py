08e8af430348b8823cad64c098c64b2776e1cd99
Ripan <none@none.com>
2024-06-08 12:01:03 -0400

    Update input.py

--- input.py
+++ input.py
@@ -1,3 +1,6 @@
+from topics import topics
+from random import randint
+
 print("""
 
 █░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   ▀█▀ █▀█   ▀█▀ █░█ █▀▀   █▀ █▀▀ █▄░█ ▀█▀ █▀▀ █▄░█ █▀▀ █▀▀   █▀▀ ▄▀█ █▀▄▀█ █▀▀
@@ -5,6 +8,8 @@ print("""
 """)
 
 plrs = input("How many players will be playing: " )
+plrStats = {}
+
 while not type(plrs) == int:
     if plrs.isnumeric():
         plrs = int(plrs)
@@ -12,3 +17,23 @@ while not type(plrs) == int:
     else:
         print("Please type a valid number!")
         plrs = input("How many players will be playing: " )
+
+for index in range(plrs):
+    index = str(index + 1)
+    plrFirstName = input("Enter Player " + index + " first name: ")
+    plrLastName = input("Enter Player " + index + " last name: ")
+    print("")
+    
+    # Account for same first and last name
+    c = 0
+    plrKey = plrFirstName + "_" + plrLastName
+
+    while plrKey in plrStats:
+        c += 1
+        plrKey = plrFirstName + "_" + plrLastName + str(c)
+
+    plrStats[plrKey] = []
+
+print(plrStats)
+
+topicLen = len(topics)

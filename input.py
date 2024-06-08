2d95d9a536fa636670bcc34554ecf545dbf8953b
Ripan <none@none.com>
2024-06-08 11:32:40 -0400

    plrs

--- input.py
+++ input.py
@@ -5,3 +5,10 @@ print("""
 """)
 
 plrs = input("How many players will be playing: " )
+while not type(plrs) == int:
+    if plrs.isnumeric():
+        plrs = int(plrs)
+        break
+    else:
+        print("Please type a valid number!")
+        plrs = input("How many players will be playing: " )

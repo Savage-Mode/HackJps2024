63cf00ece321fb9d61df6d8e4aa3ea7f9f4195aa
Ripan <none@none.com>
2024-06-08 11:34:08 -0400

    Update topics.py

--- topics.py
+++ topics.py
@@ -1,1 +1,1 @@
-# topics = ["apple", "banana", "mars", "life", "moon","buckshot roulette", "ai crap", "toilets", "planes", "trains", "pig", "sheep", "corn", "rabbits", "dogs", "new", "mewing", "height", "weight", "stress", "physicality"]
+topics = ["apple", "banana", "mars", "life", "moon","buckshot roulette", "ai crap", "toilets", "planes", "trains", "pig", "sheep", "corn", "rabbits", "dogs", "new", "mewing", "height", "weight", "stress", "physicality"]

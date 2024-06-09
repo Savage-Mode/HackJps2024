c5002e31277d8ec32bf7084f5aa88f0fd1f65800
SkibidimasterOG <120259300+StarBubbel@users.noreply.github.com>
2024-06-09 16:33:39 -0400

    Added coolstuf.html to the website.

--- websitefordathing.py
+++ websitefordathing.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__, static_url_path='/static') 
 
-htmlcode = "<p>hello people, its a me, ashwin</p>"
+
 
 @app.route("/")
 def hello_world():
-    return htmlcode
+    return render_template('coolstuf.html')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
